0829441e6d30966dd81e419c500e28b9e3abd192
anggunalmas <anggunalmas8@gmail.com>
2025-11-29 21:03:05 -0800

    Add files via upload

---
+++ praktikum3.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n; 
+    cout << "masukkan jumlah baris pascal: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+
+        for (int s = 0; s < n - i; s++) {
+            cout << " ";
+        }
+
+        int val = 1;
+        for (int j = 0; j <= i; j++) {
+
+
+            val = val * (i - j) / (j + 1);
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
